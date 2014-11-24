7efde7ff1148c2c34cc44075c05f5debdb52cc7d
Christopher Millward <christopher.millward@gmail.com>
2014-11-24 13:14:53 -0500

    Added placeholder GUI for tracking distance travelled.

---
+++ GameProject/Assets/Scripts/GameGUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameGUI : MonoBehaviour {
+	GameObject player;
+	float playerStartXCoord;	// Player's starting x-coordinate.
+	float playerDistance;
+
+	void Start() {
+		player = GameObject.FindGameObjectWithTag("Player");
+		if (!player) {
+			Debug.LogError ("Unable to initialize GameGUI: No game object has the Player tag.");
+		}
+
+		playerStartXCoord = player.transform.position.x;
+		playerDistance = 0;
+	}
+
+	/// <summary>
+	/// Renders the GUI.
+	/// </summary>
+	void OnGUI() {
+		// Make a background box.
+		GUI.Box(new Rect(10, 10, 200, 50), "Stats");
+
+		// Print the distance travelled.
+		if (player) {
+			playerDistance = player.transform.position.x - playerStartXCoord;
+		}
+		GUI.Label (new Rect(20, 30, 150, 20), string.Format ("Distance: {0:0}", playerDistance));
+	}
+}
