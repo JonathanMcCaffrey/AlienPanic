5621ea94a5099722d95687989bbe42680829ea2d
Christopher Millward <christopher.millward@gmail.com>
2014-11-25 07:45:47 -0500

    Added Geyser obstacle.

---
+++ GameProject/Assets/Scripts/Geyser.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Geyser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		Health health = col.collider.GetComponent<Health>();
+		if (health) {
+			health.ChangeHealth(-1);
+		}
+	}
+}
