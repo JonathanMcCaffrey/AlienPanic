57d87bef2749566ed7bc396b758d75084dde1ee9
Christopher Millward <christopher.millward@gmail.com>
2014-11-25 07:45:22 -0500

    Added current velocity to Game GUI.

--- GameProject/Assets/Scripts/GameGUI.cs
+++ GameProject/Assets/Scripts/GameGUI.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class GameGUI : MonoBehaviour {
 	GameObject player;
 	float playerStartXCoord;	// Player's starting x-coordinate.
-	float playerDistance;
+	float playerDistance;		// Distance travelled since the start of the game.
+	Vector2 playerVelocity;		// Player's current velocity.
 
 	void Start() {
 		player = GameObject.FindGameObjectWithTag("Player");
@@ -14,19 +15,22 @@ public class GameGUI : MonoBehaviour {
 
 		playerStartXCoord = player.transform.position.x;
 		playerDistance = 0;
+		playerVelocity = new Vector2();
 	}
 
 	/// <summary>
 	/// Renders the GUI.
 	/// </summary>
 	void OnGUI() {
-		// Make a background box.
-		GUI.Box(new Rect(10, 10, 200, 50), "Stats");
-
-		// Print the distance travelled.
+		// Update the player stats.
 		if (player) {
 			playerDistance = player.transform.position.x - playerStartXCoord;
+			playerVelocity = new Vector2(player.rigidbody2D.velocity.x, player.rigidbody2D.velocity.y);
 		}
+
+		// Render the GUI.
+		GUI.Box(new Rect(10, 10, 200, 70), "Stats");
 		GUI.Label (new Rect(20, 30, 150, 20), string.Format ("Distance: {0:0}", playerDistance));
+		GUI.Label (new Rect(20, 50, 150, 20), string.Format ("Velocity: ({0:0.0}, {1:0.0})", playerVelocity.x, playerVelocity.y));
 	}
 }
