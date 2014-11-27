6cf437aac34280f64760e074def27a118250c0cc
Shreyans Dhaddha <shreyansdhaddha@gmail.com>
2014-11-27 02:03:18 -0500

    Added Obstacle-Birds (Dynamically Create, Auto Destroy, Animate) and Custom Options for birds modifications.

---
+++ GameProject/Assets/Scripts/Demo/DemoBird.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoBird {
+
+	private GameObject bird;
+	//private bool hasRendered = false;
+	public bool isBirdDestroyed = false;
+
+	//Should not be used.
+	public DemoBird(){
+		//Unity is not allowing to remove this constructor.
+		//Dont want to load the prefab using Resouces.Load because then the directory structure would need to be changed.
+		bird = null;
+	}
+
+	public DemoBird(GameObject birdPrefab, string birdsContainer){
+		createBird(birdPrefab, birdsContainer);
+	}
+
+	private void createBird(GameObject birdPrefab, string birdsContainer){
+		bird = GameObject.Instantiate(birdPrefab) as GameObject;
+
+		//Add it inside the parent container
+		if(birdsContainer != null)
+			bird.transform.parent = GameObject.Find(birdsContainer).transform;
+
+		bird.transform.position = new Vector2(Random.Range(-5,5), Random.Range(10,20));
+
+		Debug.Log("Bird Created");
+	}
+
+	public void updateBirdMovement(float birdSpeed)
+	{
+		bird.transform.position = new Vector2((float)(bird.transform.position.x - birdSpeed), bird.transform.position.y);
+		//if(bird.renderer.isVisible)
+			//hasRendered = true;
+	}
+
+	public void destroyBird()
+	{
+		//Get the camera bounds to destroy if the object is outside the camera bounds.
+		Bounds currentBounds = bounds(Camera.main.camera);
+
+		//Destory if offScreen
+		//if((!(bird.renderer.isVisible) && hasRendered == true))
+		if(bird.transform.position.x <= (currentBounds.center.x - currentBounds.extents.x))
+		{
+			GameObject.Destroy(bird);
+			isBirdDestroyed = true;
+			Debug.Log("Bird Destroyed");
+		}
+	}
+
+	//This needs to be removed from here to a sepearte class.
+	private Bounds bounds(Camera camera)
+	{
+		//Fill the default main camera
+		if(camera == null)
+			camera = Camera.main.camera;
+		
+		float screenAspect = (float)Screen.width / (float)Screen.height;
+		float cameraHeight = Camera.main.camera.orthographicSize * 2;
+		Bounds bounds = new Bounds(Camera.main.camera.transform.position,
+		                           new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+		
+		return bounds;
+	}
+}
+
+
+
+
---
+++ GameProject/Assets/Scripts/Demo/DemoBirdController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoBirdController : MonoBehaviour {
+
+	//Editor UI Elements
+	public GameObject birdsPrefab;
+	[Range(0.01f, 0.10f)]
+	public float birdsFrequency;
+	[Range(1, 10)]
+	public int numberOfBirdsInGroup;
+	[Range(0.1f, 1.0f)]
+	public float birdsMovementSpeed;
+	[Range(1, 5)]
+	public int birdsRelaxPeriod;
+
+	//Internal variables
+	private int birdsPassedCounter = 0;
+	private ArrayList birdsArray = new ArrayList();
+
+	void Awake()
+	{
+		Random.seed = (int)Time.time;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		createBirds();
+		updateBirds();
+		destroyBirds();
+	}
+
+	void createBirds(){
+		float freq = (float)birdsFrequency;
+		float randVal = Random.value;
+		if(randVal < freq && birdsRelaxPeriod > birdsPassedCounter)
+		{
+			DemoBird bird = new DemoBird(birdsPrefab, gameObject.name);
+			birdsArray.Add(bird);
+			birdsPassedCounter++;
+		}
+	}
+
+	void updateBirds()
+	{
+		for(int i=0; i < birdsArray.Count; i++)
+		{
+			(birdsArray[i] as DemoBird).updateBirdMovement(birdsMovementSpeed);
+		}
+	}
+
+	void destroyBirds()
+	{
+		for(int i=0; i < birdsArray.Count; i++)
+		{
+			(birdsArray[i] as DemoBird).destroyBird();
+			if((birdsArray[i] as DemoBird).isBirdDestroyed)
+				birdsArray.RemoveAt(i);
+		}		
+	}
+}
+
+
+
+
