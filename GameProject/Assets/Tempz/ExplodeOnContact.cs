b46085325fe5f069fc46aa085d425a60aaa5977d
ashuv <andre@shuvamac.workgroup>
2015-01-19 16:46:31 -0500

    Added FXmaker effects. Player movement (in gameStatePlaying) is broken-ish, ignore that for now.

---
+++ GameProject/Assets/Tempz/ExplodeOnContact.cs
@@ -0,0 +1,25 @@
+/* ExplodeOnContact.cs
+ * 
+ * Display specified explosion. Destroys this object without concequences.
+ * Does not harm the player or affect the score.
+ * 
+ */
+
+
+using UnityEngine;
+using System.Collections;
+
+public class ExplodeOnContact : MonoBehaviour
+{
+	public GameObject explosion;
+	
+	void OnCollisionEnter2D (Collision2D other) {
+		print("DestroyByContact::OnCollisionEnter2D()");
+
+		if (other.gameObject.tag == "Player") {
+			Instantiate(explosion, gameObject.transform.position, gameObject.transform.rotation);
+		}
+
+		Destroy (gameObject);
+	}
+}
