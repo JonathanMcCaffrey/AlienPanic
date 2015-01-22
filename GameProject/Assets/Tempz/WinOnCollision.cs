c30e51378747d300613bcbb71d592f4e3daa181e
Andre Shuvalov <ashuvalov@gmail.com>
2015-01-22 00:28:30 -0500

    Got rid of WFXM, cleaned up FXMaker resources, added several explosions into FXMaker/Effects

---
+++ GameProject/Assets/Tempz/WinOnCollision.cs
@@ -0,0 +1,15 @@
+﻿// WinOnCollision.cs
+//
+// Attach this script to a GameObject with a BoxCollider 2D to change the game state to stateGameWon
+
+using UnityEngine;
+using System.Collections;
+
+public class WinOnCollision : MonoBehaviour {
+
+	void  OnCollisionEnter2D (Collision2D other) {
+		print ("WinOnCollision::OnCollisionEnter2D() " + other.gameObject.name);
+		
+		GameManager.instance.NewGameState(GameManager.instance.stateGameWon);
+	}
+}
