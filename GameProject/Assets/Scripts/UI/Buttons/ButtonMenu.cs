a3649e1e1646ea323643e1a4277c15ccaecadafa
Christopher Millward <christopher.millward@gmail.com>
2015-03-01 14:34:21 -0500

    Just a bit of code tidying for button handlers.

--- GameProject/Assets/Scripts/UI/Buttons/ButtonMenu.cs
+++ GameProject/Assets/Scripts/UI/Buttons/ButtonMenu.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class ButtonMenu : MonoBehaviour {
 
 	public void onClick() {
-		Application.LoadLevel ("MainMenu");
 		//TODO Goto main menu or level select
+		Application.LoadLevel ("MainMenu");
 	}
 }
--- GameProject/Assets/Scripts/UI/Buttons/ButtonPlayGame.cs
+++ GameProject/Assets/Scripts/UI/Buttons/ButtonPlayGame.cs
@@ -2,14 +2,9 @@
 using System.Collections;
 
 public class ButtonPlayGame : MonoBehaviour {
-	public GameObject mainMenu = null;
-	
-	
+
 	public void onClick() {
-
-		GameManager.instance.NewGameState(GameManager.instance.stateNewGame);
-
-		//Application.LoadLevel ("Level_1");
 		//TODO Goto first level. replace later with goto level select
+		GameManager.instance.NewGameState(GameManager.instance.stateNewGame);
 	}
 }
